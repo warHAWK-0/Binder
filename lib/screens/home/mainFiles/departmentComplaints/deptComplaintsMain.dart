29257feaa263c55483fb2674e5d164cb25ab6e04
srj940 <sj.bhatt.000@gmail.com>
2020-06-12 22:01:40 +0530

    maintenance dept supervisor

--- lib/screens/home/mainFiles/departmentComplaints/deptComplaintsMain.dart
+++ lib/screens/home/mainFiles/departmentComplaints/deptComplaintsMain.dart
@@ -22,126 +22,45 @@ class maintenanceDeptComplaints extends StatefulWidget {
 class _maintenanceDeptComplaintsState extends State<maintenanceDeptComplaints> {
   final List<int> complaintNo = [1,2,3,4,5,6];
   List<myData> allData = [];
+  List<String> documentNames = [];
 
-  @override
-  void initState(){
-    print(1);
-
-    fetchDepartmentComplaints();
-  }
   void fetchDepartmentComplaints() async {
     Color cstatus;
-
     final QuerySnapshot usersList =
     await Firestore.instance.collection('binder').getDocuments();
+    await Firestore.instance.collection('binder').getDocuments();
     final List<DocumentSnapshot> docUsers = usersList.documents;
-    allData.clear();
-    for (DocumentSnapshot docUser in docUsers) {
-      String uidUser = docUser.documentID;
-      print(uidUser);
-      final QuerySnapshot userComplaints = await Firestore.instance
-          .collection('binder')
-          .document(uidUser)
-          .collection('complaint')
-          .getDocuments();
-      final List<DocumentSnapshot> docComplaints = userComplaints.documents;
-      for (DocumentSnapshot docComplaint in docComplaints) {
-        print(docComplaint.documentID + " => " + docComplaint.data['issue']);
-        if(docComplaint.data['status']=='solved'){
-          cstatus=complaintStatusSolved;
-        }
-        else if (docComplaint.data['status']=='ongoing'){
-          cstatus=complaintStatusOngoing;
-        }
-        else if(docComplaint.data['status']=='notsolved'){
-          cstatus=complaintStatusNotSolved;
-        }
-        else if(docComplaint.data['status']=='pending'){
-          cstatus=complaintStatusPending;
-        }
-        else if(docComplaint.data['status']=='transferAME'){
-          cstatus=complaintStatusAME;
-        }
-        myData d = new myData(
-            docComplaint.data['issue'],
-            docComplaint.data['machineNo'],
-            docComplaint.data['startDate'],
-            docComplaint.data['department'],
-            docComplaint.documentID,
-        cstatus);
-        allData.add(d);
-      }
+    documentNames.clear();
+    for(DocumentSnapshot documentSnapshot in docUsers){
+      documentNames.add(documentSnapshot.documentID.toString());
     }
-    setState(() {
-      print(allData.length);
-    });
   }
+  int count =1;
 
   @override
   Widget build(BuildContext context) {
+    print(count++);
+    fetchDepartmentComplaints();
+    print("document len: "+documentNames.length.toString());
     return Scaffold(
       backgroundColor: Color(0xFFE5E5E5),
       appBar: CustomAppBar(backIcon: false, child: Text('Department Complaints',style: titleText,)),
 //
-      body: StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection("binder").snapshots(),
-        // ignore: missing_return
-        builder: (context,snapshot) {
-          if(! snapshot.hasData){
-            return Container(child: Text('not has data'),);
-          }
-          else{
-            int index = snapshot.data.documents.length;
-            final List<DocumentSnapshot> userDocs = snapshot.data.documents;
-            for (DocumentSnapshot snap in userDocs) {
-             return StreamBuilder<QuerySnapshot>(
-                stream: Firestore.instance.collection("binder").document(snap.documentID).collection("complaints").snapshots(),
-                // ignore: missing_return
-                builder: (context,snap){
-                  if(! snap.hasData){
-                    return Loading();
-                  }
-                  else{
-                    return ListView.builder(
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (_, index) {
-                        DocumentSnapshot data=snapshot.data.documents[index];
-                        Color cstatus;
-                        print(data['issue']);
-                        if(data['status']=='solved'){
-                          cstatus=complaintStatusSolved;
-                        }
-                        else if (data['status']=='ongoing'){
-                          cstatus=complaintStatusOngoing;
-                        }
-                        else if(data['status']=='notsolved'){
-                          cstatus=complaintStatusNotSolved;
-                        }
-                        else if(data['status']=='pending'){
-                          cstatus=complaintStatusPending;
-                        }
-                        else if(data['status']=='transferAME') {
-                          cstatus = complaintStatusAME;
-                        }
-                        return CustomComplaintCard(
-                          userDetails: widget.userDetails,
-                          complaintNo: data.documentID,
-                          title: data["issue"],
-                          machineno: data["machineno"],
-                          date: data["startDate"],
-                          department: data["department"],
-                          cstatus: cstatus,
-
-                        );
-                      },
-                    );
-                    return Container(child: Text('has data'),);
-                  }
-                },
-              );
+      body: ListView.builder(
+        itemCount: documentNames.length,
+        itemBuilder: (_,index) => StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection("binder").document(documentNames[index]).collection("complaint").snapshots(),
+          builder: (context,snapshot){
+            if(!snapshot.hasData){
+              print('loading');
+              return Loading();
             }
-          }
-        },
+            else{
+              print('here\n\n\n\n');
+              return snapshot.hasData == false ? Text('no data') : Text(snapshot.data.documents[0]['issue']);
+            }
+          },
+        ),
       ),
     );
   }
